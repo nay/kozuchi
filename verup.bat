9db5443317f6d81db2c4a45298857113df814ae2
koichiro <koichiro@c938ebee-df10-0410-95fc-be8c4b0ebdd5>
2006-05-02 11:35:01 +0000

    Added version up script.
    git-svn-id: svn+ssh://ko.meadowy.net/home/svn/inside/kozuchi/trunk@112 c938ebee-df10-0410-95fc-be8c4b0ebdd5

---
+++ verup.bat
@@ -0,0 +1,13 @@
+@echo off
+
+echo "%1"
+
+if "" == "%1" goto END
+
+rem svn ls http://localhost:9000/repos-inside/kozuchi/tags/%1
+
+svn copy http://localhost:9000/repos-inside/kozuchi/trunk http://localhost:9000/repos-inside/kozuchi/tags/%1 -m "version up %1"
+
+:END
+
+echo done
