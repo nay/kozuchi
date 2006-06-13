7fb99732a555229537b7a35a2cf435029f3f6cd7
koichiro <koichiro@c938ebee-df10-0410-95fc-be8c4b0ebdd5>
2006-06-13 05:51:25 +0000

    fix GEM_VERSION.
    git-svn-id: svn+ssh://ko.meadowy.net/home/svn/inside/kozuchi/trunk@193 c938ebee-df10-0410-95fc-be8c4b0ebdd5

--- config/environment.rb
+++ config/environment.rb
@@ -7,7 +7,7 @@ $KCODE = 'u'
 # ENV['RAILS_ENV'] ||= 'production'
 
 # Specifies gem version of Rails to use when vendor/rails is not present
-RAILS_GEM_VERSION = '1.1.2'
+# RAILS_GEM_VERSION = '1.1.2'
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
