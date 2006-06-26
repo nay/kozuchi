efbb347182973f12b0e7fb015f13bccd6cd1aa12
nay <nay@c938ebee-df10-0410-95fc-be8c4b0ebdd5>
2006-06-26 07:53:57 +0000

    git-svn-id: svn+ssh://ko.meadowy.net/home/svn/inside/kozuchi/trunk@224 c938ebee-df10-0410-95fc-be8c4b0ebdd5

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -17,6 +17,33 @@ module ApplicationHelper
   def format_deal(deal)
     return "記入 #{deal.date}-#{deal.daily_seq}"
   end
+  
+  # 円グラフのためのURLを表示する
+  def pie_graph(resource_array, percentage_method, name_method, value_method = nil, total_value = nil)
+    labels = ''
+    values = ''
+    begin
+      for r in resource_array
+        if percentage_method
+          percentage = r.method(percentage_method).call
+        else
+          percentage = total_value != 0 ? (r.method(value_method).call * 100.0 / total_value).round : 0
+        end
+        next if percentage == 100 || percentage == 0
+        values += percentage.to_s + ','
+        labels += r.method(name_method).call
+      end
+    
+      if labels.count(',') > 1
+        url = url_for(:controller=>'graph', :action=>'pie', :labels=>labels, :values=>values)
+        return '<img src="' + url + ' />"'
+      else
+        return '<span>グラフは２つ以上の項目があるときに表示されます。</span>'
+      end
+    rescue => err
+      return "<span>エラーのためグラフを表示できません。<br />#{err}<br />#{err.backtrace}</span>"
+    end
+  end
 
 class AccountGroup
   attr_reader :name, :accounts
