c146d9833987dae8d056de2086df3bca6b3728d5
nay <nay@c938ebee-df10-0410-95fc-be8c4b0ebdd5>
2007-04-14 06:21:27 +0000

    git-svn-id: svn+ssh://ko.meadowy.net/home/svn/inside/kozuchi/trunk@322 c938ebee-df10-0410-95fc-be8c4b0ebdd5

---
+++ db/migrate/005_change_account_type.rb
@@ -0,0 +1,11 @@
+class ChangeAccountType < ActiveRecord::Migration
+  def self.up
+    rename_column(:accounts, :account_type, :account_type_code)
+    rename_column(:accounts, :asset_type, :asset_type_code)
+  end
+
+  def self.down
+    rename_column(:accounts, :account_type_code, :account_type)
+    rename_column(:accounts, :asset_type_code, :asset_type)
+  end
+end
