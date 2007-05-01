7431c582397909acef155d73827a7e18912f8311
nay <nay@c938ebee-df10-0410-95fc-be8c4b0ebdd5>
2007-05-01 01:48:26 +0000

    fixtureの更新に対応。
    git-svn-id: svn+ssh://ko.meadowy.net/home/svn/inside/kozuchi/trunk@356 c938ebee-df10-0410-95fc-be8c4b0ebdd5

--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -5,19 +5,15 @@ class UserTest < Test::Unit::TestCase
 
   # account.types_in が正しく動作することのテスト
   def test_accounts_types_in
-    assert_equal 2, @test_user_1.accounts.types_in(:asset).size
+    assert_equal 4, @test_user_1.accounts.types_in(:asset).size
     assert_equal 0, @test_user_1.accounts.types_in.size
     assert_equal 3, @test_user_1.accounts.types_in(:asset, :expense).size
   end
   
   def test_accounts_asset_types_in
     assert_equal 1, @test_user_1.accounts.asset_types_in(:cache).size
-    assert_equal 0, @test_user_1.accounts.asset_types_in(:banking_facility).size
+    assert_equal 1, @test_user_1.accounts.asset_types_in(:banking_facility).size
     assert_equal 2, @test_user_1.accounts.asset_types_in(:cache, :credit).size
   end
 
-  # Replace this with your real tests.
-  def test_create
-    assert true
-  end
 end
