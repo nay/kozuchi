959ee95413a66b441e84be95207c529366e93427
nay <nay@c938ebee-df10-0410-95fc-be8c4b0ebdd5>
2007-06-24 10:28:23 +0000

    git-svn-id: svn+ssh://ko.meadowy.net/home/svn/inside/kozuchi/trunk@393 c938ebee-df10-0410-95fc-be8c4b0ebdd5

--- db/migrate/007_add_type_to_accounts.rb
+++ db/migrate/007_add_type_to_accounts.rb
@@ -1,6 +1,6 @@
 class AddTypeToAccounts < ActiveRecord::Migration
   def self.up
-#    add_column(:accounts, :type, :text)
+    add_column(:accounts, :type, :text)
     execute("update accounts set type = 'Income' where account_type_code = 3;")
     execute("update accounts set type = 'Expense' where account_type_code = 2;")
     execute("update accounts set type = 'Cache' where account_type_code = 1 and asset_type_code = 1;")
