2123e779036df0cce42b0ab8ff95f61a142f495c
nay <nay@c938ebee-df10-0410-95fc-be8c4b0ebdd5>
2007-06-30 07:51:50 +0000

    fix capital fund data change bug.
    git-svn-id: svn+ssh://ko.meadowy.net/home/svn/inside/kozuchi/trunk@408 c938ebee-df10-0410-95fc-be8c4b0ebdd5

---
+++ db/migrate/009_modify_capital_fund_data.rb
@@ -0,0 +1,9 @@
+class ModifyCapitalFundData < ActiveRecord::Migration
+  def self.up
+    execute("update accounts set type = 'CapitalFund' where type = 'CaptitalFund';")
+  end
+
+  def self.down
+    execute("update accounts set type = 'CaptitalFund' where type = 'CapitalFund';")
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -2,7 +2,7 @@
 # migrations feature of ActiveRecord to incrementally modify your database, and
 # then regenerate this schema definition.
 
-ActiveRecord::Schema.define(:version => 8) do
+ActiveRecord::Schema.define(:version => 9) do
 
   create_table "account_entries", :force => true do |t|
     t.column "user_id", :integer, :default => 0, :null => false
