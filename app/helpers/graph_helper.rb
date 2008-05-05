92e87aa9b491898bcfd1f2bea4d1af7aa6e861fe
koichiro <koichiro@c938ebee-df10-0410-95fc-be8c4b0ebdd5>
2008-05-05 21:55:18 +0000

    パイグラフの描画をGdChartをやめてHTML5JP Circle Graphに変更する。
    git-svn-id: svn+ssh://ko.meadowy.net/home/svn/inside/kozuchi/trunk@501 c938ebee-df10-0410-95fc-be8c4b0ebdd5

--- app/helpers/graph_helper.rb
+++ app/helpers/graph_helper.rb
@@ -1,30 +1,52 @@
 module GraphHelper
 
-# 円グラフのためのURLを表示する
-  def pie_graph(resource_array, percentage_method, name_method, value_method = nil, total_value = nil)
-    labels = ''
-    values = ''
+# HTML5.JP glaph_circle を用いて円グラフを描画する
+  def pie_graph(resources, percentage_method, name_method, value_method = nil, total_value = nil, id = "graph")
+    data = {}
     begin
-      for r in resource_array
+      for r in resources
         if percentage_method
           percentage = r.method(percentage_method).call
         else
           percentage = total_value != 0 ? (r.method(value_method).call * 100.0 / total_value).round : 0
         end
         next if percentage == 100 || percentage == 0
-        values += percentage.to_s + ','
-        labels += r.method(name_method).call + ','
+        data[r.method(name_method).call] = percentage.to_s
       end
 
-      if labels.count(',') > 1
-        url = url_for(:controller=>'graph', :action=>'pie', :labels=>labels, :values=>values)
-        return '<img width="300" height="200" src="' + url + '" />'
+      if data.size > 1
+#        url = url_for(:controller=>'graph', :action=>'pie', :labels=>labels, :values=>values)
+#        return '<img width="300" height="200" src="' + url + '" />'
+        return render_javascript_pie( id, data )
       else
         return '<span>グラフは２つ以上の項目があるときに表示されます。</span>'
       end
     rescue => err
       return "<span>エラーのためグラフを表示できません。<br />#{err}<br />#{err.backtrace}</span>"
     end
+  end
+  
+  private
+  def render_javascript_pie id, data
+    s = javascript_tag <<-EOS
+Event.observe(window, 'load', function () {
+	var cg = new html5jp.graph.circle("#{id}");
+	if( ! cg ) { return; }
+	var items = [
+        #{ to_items( data ) }
+	];
+	cg.draw(items);
+}, false);
+    EOS
+    s << %Q(<div><canvas width="300" height="200" id="#{id}"></canvas></div>)
+  end
+  
+  def to_items( data )
+    r = ''
+    data.each do |key, value|
+      r << %Q(["#{key}", #{value}],\n)
+    end
+    r.chomp!(",\n")
   end
 
 end
