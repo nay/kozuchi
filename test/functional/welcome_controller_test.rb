87577fd110347bf485fd78c565ac4b75504c9591
nay <nay@c938ebee-df10-0410-95fc-be8c4b0ebdd5>
2008-05-26 08:32:55 +0000

    git-svn-id: svn+ssh://ko.meadowy.net/home/svn/inside/kozuchi/trunk@543 c938ebee-df10-0410-95fc-be8c4b0ebdd5

--- test/functional/welcome_controller_test.rb
+++ test/functional/welcome_controller_test.rb
@@ -11,4 +11,12 @@ class WelcomeControllerTest < ActionController::TestCase
     # ログインフォームがあること
     assert_tag :input, :attributes => {:id => 'login'}
   end
+  
+  def test_index_with_login
+    get :index, nil, :user_id => users(:quentin).id
+    assert_response :success
+    
+    # ログインフォームがないこと
+    assert_no_tag :input, :attributes => {:id => 'login'}
+  end
 end
