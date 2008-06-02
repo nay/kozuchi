718acf339510ccc85bc230983975453eff74e414
nay <nay@c938ebee-df10-0410-95fc-be8c4b0ebdd5>
2008-06-02 08:59:34 +0000

    git-svn-id: svn+ssh://ko.meadowy.net/home/svn/inside/kozuchi/trunk@557 c938ebee-df10-0410-95fc-be8c4b0ebdd5

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,7 @@
 class HomeController < ApplicationController
   
   def index
+    # TODO:単位をなんとか
     today = Date.today
     @monthly_expense = @user.expense_summary(today.year, today.month)
     @assets_sum, @pure_assets_sum = @user.assets_summary(today.year, today.month)
@@ -9,7 +10,9 @@ class HomeController < ApplicationController
     @pure_assets_item = ["純資産"]
     @months_for_expenses = ["月"]
     @months_for_assets = ["月"]
-    @assets_y_min = 0
+    @assets_y_min = @assets_sum
+    @assets_y_min = @pure_assets_sum if @pure_assets_sum < @assets_y_min
+    @assets_y_max = @assets_sum
     # 前５ヶ月分を計算
     day = today << 5
     while(@expenses_item.size < 6)
@@ -19,6 +22,7 @@ class HomeController < ApplicationController
       assets_sum, pure_assets_sum = @user.assets_summary(day.year, day.month)
       @assets_y_min = assets_sum if assets_sum < @assets_y_min
       @assets_y_min = pure_assets_sum if pure_assets_sum < @assets_y_min
+      @assets_y_max = assets_sum if assets_sum > @assets_y_max
       @assets_item << assets_sum
       @pure_assets_item << pure_assets_sum
       day = day >> 1
@@ -28,8 +32,11 @@ class HomeController < ApplicationController
     @pure_assets_item << @pure_assets_sum
     @months_for_expenses << "#{today.month}月"
     @months_for_assets << "#{today.month}月"
-    @assets_y_min = @assets_sum if @assets_sum < @assets_y_min
-    @assets_y_min = @pure_assets_sum if @pure_assets_sum < @assets_y_min
+    # TODO: 変化の幅に応じてグラフの詳細を記述する
+    margin = (@assets_y_max - @assets_y_min) / 10
+    @assets_y_max += margin
+    @assets_y_min -= margin
+
 #    today = Date.today
 #    redirect_to :controller => 'deals', :year => today.year, :month => today.month
   end
