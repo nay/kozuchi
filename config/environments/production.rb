8eb3c8f5fdb8a78629c212367c60d1fe92c8555b
nay <nay@c938ebee-df10-0410-95fc-be8c4b0ebdd5>
2008-06-03 07:33:45 +0000

    git-svn-id: svn+ssh://ko.meadowy.net/home/svn/inside/kozuchi/trunk@565 c938ebee-df10-0410-95fc-be8c4b0ebdd5

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -22,46 +22,47 @@ config.action_controller.perform_caching             = true
 require 'error_handler_basic' # defines AC::Base#rescue_action_in_public
 
 
-class << Dispatcher
-  def dispatch(cgi = CGI.new,
-               session_options = ActionController::CgiRequest::DEFAULT_SESSION_OPTIONS)
-    begin
-      request, response =
-        ActionController::CgiRequest.new(cgi, session_options),
-        ActionController::CgiResponse.new(cgi)
-      prepare_application
-      ActionController::Routing::Routes.recognize!(request).process(request, response).out
-    rescue Object => exception
-      begin
-        ActionController::Base.process_with_exception(request, response, exception).out
-      rescue ActionController::RoutingError
-        # pass
-      rescue ActionController::UnknownAction
-        # pass
-      rescue
-        # The rescue action above failed also, probably for the same reason
-        # the original action failed.  Do something simple which is unlikely
-        # to fail.  You might want to redirect to a static page instead of this.
-        e = exception
-        ExceptionMailer.deliver_emergency(e)
-
-        cgi.header("type" => "text/html")
-        cgi.out('cookie' => '') do
-          <<-RESPONSE
-    <html>
-      <head><title>Application Error</title></head>
-      <body>
-        <h1>Application Error</h1>
-        <b><pre>#{e.class}: #{e.message}</pre></b>
-
-        <pre>#{e.backtrace.join("\n")}</pre>
-      </body>
-    </html>
-        RESPONSE
-        end
-      end
-    ensure
-      reset_application
-    end
-  end
-end
+# 2.0対応時に動かなくなったのでコメントアウト
+#class << Dispatcher
+#  def dispatch(cgi = CGI.new,
+#               session_options = ActionController::CgiRequest::DEFAULT_SESSION_OPTIONS)
+#    begin
+#      request, response =
+#        ActionController::CgiRequest.new(cgi, session_options),
+#        ActionController::CgiResponse.new(cgi)
+#      prepare_application
+#      ActionController::Routing::Routes.recognize!(request).process(request, response).out
+#    rescue Object => exception
+#      begin
+#        ActionController::Base.process_with_exception(request, response, exception).out
+#      rescue ActionController::RoutingError
+#        # pass
+#      rescue ActionController::UnknownAction
+#        # pass
+#      rescue
+#        # The rescue action above failed also, probably for the same reason
+#        # the original action failed.  Do something simple which is unlikely
+#        # to fail.  You might want to redirect to a static page instead of this.
+#        e = exception
+#        ExceptionMailer.deliver_emergency(e)
+#
+#        cgi.header("type" => "text/html")
+#        cgi.out('cookie' => '') do
+#          <<-RESPONSE
+#    <html>
+#      <head><title>Application Error</title></head>
+#      <body>
+#        <h1>Application Error</h1>
+#        <b><pre>#{e.class}: #{e.message}</pre></b>
+#
+#        <pre>#{e.backtrace.join("\n")}</pre>
+#      </body>
+#    </html>
+#        RESPONSE
+#        end
+#      end
+#    ensure
+#      reset_application
+#    end
+#  end
+#end
