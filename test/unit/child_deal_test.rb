02a1a3b3d1091a9facc5cc0596f0e2181ea358f5
nay <nay@c938ebee-df10-0410-95fc-be8c4b0ebdd5>
2008-06-26 02:45:34 +0000

    git-svn-id: svn+ssh://ko.meadowy.net/home/svn/inside/kozuchi/trunk@609 c938ebee-df10-0410-95fc-be8c4b0ebdd5

--- test/unit/child_deal_test.rb
+++ test/unit/child_deal_test.rb
@@ -1,20 +1,14 @@
 require File.dirname(__FILE__) + '/../test_helper'
 
 class ChildDealTest < Test::Unit::TestCase
-#  self.use_instantiated_fixtures  = true
-#  fixtures :users
-#  fixtures 'account/accounts'.to_sym
-#  set_fixture_class  "account/accounts".to_sym => 'account/base'
-#  fixtures :account_rules
 
   # 精算ルールがあるとき、従属行が新しく作られ、更新時は作り直されることをテストする
   def test_create
-    # なぜかuse_instantiated_fixturesが効かない
-    @first_credit_card = Account::Base.find(6)
-    @first_food = Account::Base.find(2)
-    @first_bank = Account::Base.find(7)
+    @first_credit_card = accounts(:first_credit_card)
+    @first_food = accounts(:first_food)
+    @first_bank = accounts(:first_bank)
     
-    deal = Deal.new(:user_id => 1, :summary => 'カードで食事', :minus_account_id => @first_credit_card.id, :plus_account_id => @first_food.id, :amount => 3200, :date => Date.today)
+    deal = Deal.new(:user_id => users(:old).id, :summary => 'カードで食事', :minus_account_id => @first_credit_card.id, :plus_account_id => @first_food.id, :amount => 3200, :date => Date.today)
     deal.save!
     assert_equal 1, deal.children.size
     child = deal.children(true)[0] # TODO: とりなおさないと失敗する
