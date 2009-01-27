56fdf1caea8000d19f8097096c0a6cca3d59e1e4
nay <nay@c938ebee-df10-0410-95fc-be8c4b0ebdd5>
2009-01-27 01:42:20 +0000

    git-svn-id: svn+ssh://ko.meadowy.net/home/svn/inside/kozuchi/trunk@704 c938ebee-df10-0410-95fc-be8c4b0ebdd5

--- db/migrate/20090103211631_add_id_to_account_links.rb
+++ db/migrate/20090103211631_add_id_to_account_links.rb
@@ -6,8 +6,18 @@ class AddIdToAccountLinks < ActiveRecord::Migration
       data << {:account_id => account_id, :user_id => user_id, :target_user_id => target_user_id, :target_ex_account_id => target_ex_account_id}
     end
     p data.inspect
-    remove_index :account_links, :connected_account_id
-#    remove_index :account_links, :name => "account_links_connected_account_id_index"
+    index_deleted = false
+    begin
+      remove_index :account_links, :connected_account_id
+      index_deleted = true
+    rescue
+    end
+    begin
+      remove_index :account_links, :name => "account_links_connected_account_id_index"
+      index_deleted = true
+    rescue
+    end
+    p "Could not remove index in any approach." unless index_deleted
    
     drop_table "account_links"
     create_table "account_links" do |t|
@@ -21,7 +31,6 @@ class AddIdToAccountLinks < ActiveRecord::Migration
     
     add_index :account_links, :account_id
     add_index :account_links, :target_ex_account_id
-#    add_index "account_links", ["target_ex_account_id"], :name => "account_links_target_ex_account_id_index"
     for hash in data
       execute("insert into account_links (user_id, account_id, target_user_id, target_ex_account_id, created_at, updated_at) values (#{hash[:user_id]}, #{hash[:account_id]}, #{hash[:target_user_id]}, #{hash[:target_ex_account_id]}, now(), now())")
     end
@@ -33,7 +42,7 @@ class AddIdToAccountLinks < ActiveRecord::Migration
     for account_id, user_id, target_user_id, target_ex_account_id in execute "select account_id, user_id, target_user_id, target_ex_account_id from account_links"
       data << {:account_id => account_id, :user_id => user_id, :target_user_id => target_user_id, :target_ex_account_id => target_ex_account_id}
     end
-    remove_index :account_links, :name => "account_links_target_ex_account_id_index"
+    remove_index :account_links, :target_ex_account_id
     drop_table "account_links"
     create_table "account_links", :id => false, :force => true do |t|
       t.integer "account_id",           :limit => 11, :null => false
@@ -43,7 +52,7 @@ class AddIdToAccountLinks < ActiveRecord::Migration
     for hash in data
       execute("insert into account_links (account_id, connected_account_id) values (#{hash[:account_id]}, #{hash[:target_ex_account_id]})")
     end
-
-    add_index "account_links", ["connected_account_id"], :name => "account_links_connected_account_id_index"
+    add_index :account_links, :connected_account_id
+#    add_index "account_links", ["connected_account_id"], :name => "account_links_connected_account_id_index"
   end
 end
