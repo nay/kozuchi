9e840828365d560b9a26502f32046589abaa5d17
nay <nay@c938ebee-df10-0410-95fc-be8c4b0ebdd5>
2009-05-07 02:35:30 +0000

    refs #50 Added usr_id to AccountLinkRequest
    git-svn-id: svn+ssh://ko.meadowy.net/home/svn/inside/kozuchi/trunk@759 c938ebee-df10-0410-95fc-be8c4b0ebdd5

--- app/models/account_link_request.rb
+++ app/models/account_link_request.rb
@@ -4,12 +4,23 @@ class AccountLinkRequest < ActiveRecord::Base
 
   validates_uniqueness_of :sender_ex_account_id, :scope => [:sender_id, :account_id]
 
+  after_save :set_user_id
+
   def sender_account
     sender.account(sender_ex_account_id)
   end
 
   def account_name_with_user
     sender.account_summary(sender_ex_account_id)[:name_with_user]
+  end
+
+  private
+  # account_id から user_id を自動的に入れる
+  def set_user_id
+    raise "no account_id" if self.account_id.blank?
+    raise "no account for id #{self.account_id}" unless self.account
+    self.user_id = account.user_id
+    self.class.update_all("user_id = #{account.user_id}", ["id = ?", self.id])
   end
 
 end
