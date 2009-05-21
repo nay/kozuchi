85f70bb2b9b4e88510a81eb520364e5c38bcec99
nay <nay@c938ebee-df10-0410-95fc-be8c4b0ebdd5>
2009-05-21 05:28:56 +0000

    deleted admin_users table
    git-svn-id: svn+ssh://ko.meadowy.net/home/svn/inside/kozuchi/trunk@787 c938ebee-df10-0410-95fc-be8c4b0ebdd5

---
+++ db/migrate/20090521044900_drop_admin_users.rb
@@ -0,0 +1,12 @@
+class DropAdminUsers < ActiveRecord::Migration
+  def self.up
+    drop_table :admin_users
+  end
+
+  def self.down
+    create_table :admin_users do |t|
+      t.column :name, :string
+      t.column :hashed_password, :string, :limit => 40
+    end
+  end
+end
