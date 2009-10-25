13d985c4b68e105444607fcc490366d6b49d2a93
nay <y.ohba@everyleaf.com>
2009-10-25 09:54:32 +0900

    refs #68

--- public/javascripts/book.js
+++ public/javascripts/book.js
@@ -124,4 +124,18 @@ Month.prototype = {
   **/
   function selectBalanceTab() {
     
-  }    
+  }
+
+ var money_counting_fields, money_counting_amounts;
+ /**
+  * function : 残高編集時に金額を計算する 
+  */
+ function countMoney() {
+   var amount = 0;
+   if (!money_counting_fields) money_counting_fields = ['man', 'gosen', 'nisen', 'sen', 'gohyaku', 'hyaku', 'gojyu', 'jyu', 'go', 'ichi'];
+   if (!money_counting_amounts) money_counting_amounts = [10000, 5000, 2000, 1000, 500, 100, 50, 10, 5, 1];
+   for(i = 0; i < money_counting_fields.length; i++) {
+     if ($(money_counting_fields[i]).value != '') amount += (parseInt($(money_counting_fields[i]).value) * money_counting_amounts[i]);
+   }
+   $('deal_balance').value = amount;
+ }
