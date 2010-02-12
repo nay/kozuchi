8d3aa883aa9fa3517e7a448a21d612642aca9948
nay <y.ohba@everyleaf.com>
2010-02-13 00:27:00 +0900

    fixed migration

--- db/migrate/20100212084008_add_linked_ex_entry_confirmed_to_account_entries.rb
+++ db/migrate/20100212084008_add_linked_ex_entry_confirmed_to_account_entries.rb
@@ -1,6 +1,6 @@
 class AddLinkedExEntryConfirmedToAccountEntries < ActiveRecord::Migration
   def self.up
-#    add_column :account_entries, :linked_ex_entry_confirmed, :boolean, :null => false, :default => false
+    add_column :account_entries, :linked_ex_entry_confirmed, :boolean, :null => false, :default => false
 
     execute "update account_entries inner join deals on account_entries.linked_ex_deal_id = deals.id set linked_ex_entry_confirmed = deals.confirmed"
   end
