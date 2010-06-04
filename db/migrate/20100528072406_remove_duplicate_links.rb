2cf6b65a290503de18b10141696a5c80621ee61d
nay <y.ohba@everyleaf.com>
2010-06-04 18:08:16 +0900

    modified migration to remove account_link_requests which has no account_link

--- db/migrate/20100528072406_remove_duplicate_links.rb
+++ db/migrate/20100528072406_remove_duplicate_links.rb
@@ -3,6 +3,11 @@ class RemoveDuplicateLinks < ActiveRecord::Migration
     for account_id, id in execute("select account_id, min(id) as count from account_links group by account_id") do
       execute("delete from account_links where account_id = #{account_id} and id != #{id}")
     end
+    invalid_account_link_requests = []
+    for id, remote_id in execute "select account_link_requests.id, account_links.id as remote_id from account_link_requests left outer join account_links on account_links.user_id = account_link_requests.sender_id and account_links.account_id = account_link_requests.sender_ex_account_id where account_links.id is null"
+      invalid_account_link_requests << id.to_s
+    end
+    execute("delete from account_link_requests where id in (#{invalid_account_link_requests.join(',')})") unless invalid_account_link_requests.empty?
   end
 
   def self.down
