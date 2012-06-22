978313fe021145e3d2be01187598d3338650b125
nay3 <y.ohba@everyleaf.com>
2012-06-22 17:25:58 +0900

    remove shared_context

--- spec/support/user_agent.rb
+++ spec/support/user_agent.rb
@@ -1,18 +1,8 @@
 # -*- encoding : utf-8 -*-
 
-# AUからのアクセス（簡単ログイン設定なし）
+# AUからのアクセス
 shared_context "requested from AU" do
   before do
-    page.driver.header("x-up-subno", "01234567890123_xx.ezweb.ne.jp")
-    page.driver.header("User-Agent", "KDDI-HI31 UP.Browser/6.2.0.5 (GUI) MMP/2.0")
-  end
-end
-
-# AUからのアクセス（簡単ログイン設定あり）
-shared_context "requested from AU with the passport" do
-  let!(:current_user) {users(:no_mobile_ident_user)}
-  before do
-    current_user.update_mobile_identity!("01234567890123_xx.ezweb.ne.jp", "KDDI-HI31 UP.Browser/6.2.0.5 (GUI) MMP/2.0")
     page.driver.header("x-up-subno", "01234567890123_xx.ezweb.ne.jp")
     page.driver.header("User-Agent", "KDDI-HI31 UP.Browser/6.2.0.5 (GUI) MMP/2.0")
   end
