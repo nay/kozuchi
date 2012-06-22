8c70bdfaa23a6d5c590f3b647a5c45c52b44dbbd
nay3 <y.ohba@everyleaf.com>
2012-06-22 10:26:14 +0900

    move asset files under app/assets. fix html5jp_graphs bug

--- vendor/plugins/html5jp_graphs/install.rb
+++ vendor/plugins/html5jp_graphs/install.rb
@@ -2,7 +2,7 @@
 
 Dir.chdir(File.join(::Rails.root.to_s, 'vendor', 'plugins', 'html5jp_graphs')) do
   dest_dir = File.join(::Rails.root.to_s, 'public', 'javascripts')
-  FileUtils.mkdirs(dest_dir) unless File.exist?(dest_dir)
+  FileUtils.mkdir(dest_dir) unless File.exist?(dest_dir)
 
   Dir.glob("html5jp/html5jp/**/*.js").each do |path|
     FileUtils.cp(path, dest_dir) unless File.exist?(File.join(dest_dir, File.basename(path)))
