55f0fed2a3ec7e5917e0bd9ef9a170ebbae0d7c0
nay3 <y.ohba@everyleaf.com>
2012-07-22 07:49:20 +0900

    add comment

--- spec/requests/deals_spec.rb
+++ spec/requests/deals_spec.rb
@@ -1,6 +1,7 @@
 # -*- encoding : utf-8 -*-
 require 'spec_helper'
 
+# TODO: とりあえずindex表示だけ仮に置いてみた。Ajax部分どうしようかな...
 describe DealsController do
   fixtures :users, :accounts, :preferences
   set_fixture_class  :accounts => Account::Base
