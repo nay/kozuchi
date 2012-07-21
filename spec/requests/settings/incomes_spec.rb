e502d9a21472c5df0485ca9a1bdc9db54baa5609
nay3 <y.ohba@everyleaf.com>
2012-07-22 08:03:24 +0900

    add request spec for settings/incomes_controller

---
+++ spec/requests/settings/incomes_spec.rb
@@ -0,0 +1,17 @@
+# -*- encoding : utf-8 -*-
+require 'spec_helper'
+
+describe Settings::IncomesController do
+  fixtures :users, :accounts, :preferences
+  set_fixture_class  :accounts => Account::Base
+
+  describe "/settings/incomes" do
+    include_context "太郎 logged in"
+    before do
+      visit "/settings/incomes"
+    end
+    it "口座名入力欄がある" do
+      page.should have_css('input#account_name')
+    end
+  end
+end
