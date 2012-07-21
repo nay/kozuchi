6cc24e9b0c94cb57fce4770b2ced233bb7677201
nay3 <y.ohba@everyleaf.com>
2012-07-22 07:44:28 +0900

    add specs for deals_controller

---
+++ spec/requests/deals_spec.rb
@@ -0,0 +1,20 @@
+# -*- encoding : utf-8 -*-
+require 'spec_helper'
+
+describe DealsController do
+  fixtures :users, :accounts, :preferences
+  set_fixture_class  :accounts => Account::Base
+
+  describe "/deals/2012/7" do
+    include_context "太郎 logged in"
+    before do
+      visit "/deals/2012/7"
+    end
+    it "明細表示領域がある" do
+      page.should have_css('div#monthly_contents')
+    end
+    it "明細入力欄がある" do
+      page.should have_css('input#deal_summary')
+    end
+  end
+end
