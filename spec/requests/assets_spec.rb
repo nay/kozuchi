7edb6c4dcde8980f3243c0f9b11964e368d4b70a
nay3 <y.ohba@everyleaf.com>
2012-11-11 05:28:45 +0900

    Add title to Assets#index. Add a spec for vsiiting there.

---
+++ spec/requests/assets_spec.rb
@@ -0,0 +1,20 @@
+# -*- encoding : utf-8 -*-
+require 'spec_helper'
+
+describe AssetsController do
+  fixtures :users, :accounts, :preferences
+  set_fixture_class  :accounts => Account::Base
+
+  include_context "太郎 logged in"
+
+  before do
+    visit '/f/assets'
+  end
+
+  describe "メニュー「資産表」のクリック" do
+    it "今月の資産表が表示される" do
+      page.should have_content("#{Date.today.year}年#{Date.today.month}月末日の資産表")
+    end
+  end
+
+end
