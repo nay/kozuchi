e0275678166e3d38514a9bad94444981910cdc68
nay3 <y.ohba@everyleaf.com>
2012-11-11 05:24:10 +0900

    Add a request spec for BalanceSheetController.

---
+++ spec/requests/balance_sheet_spec.rb
@@ -0,0 +1,20 @@
+# -*- encoding : utf-8 -*-
+require 'spec_helper'
+
+describe BalanceSheetController do
+  fixtures :users, :accounts, :preferences
+  set_fixture_class  :accounts => Account::Base
+
+  include_context "太郎 logged in"
+
+  before do
+    visit 'balance_sheet'
+  end
+
+  describe "メニュー「貸借対照表」のクリック" do
+    it "今月の貸借対照表が表示される" do
+      page.should have_content("#{Date.today.year}年#{Date.today.month}月末日の貸借対照表")
+    end
+  end
+
+end
