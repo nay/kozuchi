6e753dd21cb9d65864c75ea19c4f0e3bb4eb7f7b
nay <y.ohba@everyleaf.com>
2012-11-30 16:19:50 +0900

    Make the migration support new STI naming rule.

--- db/migrate/20121128171301_add_creditor_and_line_number_to_account_entries.rb
+++ db/migrate/20121128171301_add_creditor_and_line_number_to_account_entries.rb
@@ -1,5 +1,30 @@
+# -*- encoding : utf-8 -*-
 class AddCreditorAndLineNumberToAccountEntries < ActiveRecord::Migration
   def up
+    # type値をRails3仕様に変える（忘れていたのでここで）
+    # これはdownでも戻さない
+
+    # account_entries
+    ['General', 'Balance'].each do |t|
+      execute "UPDATE account_entries set type = 'Entry::#{t}' WHERE type = '#{t}'"
+    end
+
+    # accounts
+    ['Asset', 'Expense', 'Income'].each do |t|
+      execute "UPDATE accounts set type = 'Account::#{t}' WHERE type = '#{t}'"
+    end
+
+    # deals
+    ['Balance', 'General'].each do |t|
+      execute "UPDATE deals set type = 'Deal::#{t}' WHERE type = '#{t}'"
+    end
+
+    # friend_permissions
+    ['Acceptance', 'Rejection'].each do |t|
+      execute "UPDATE friend_permissions set type = 'Friend::#{t}' WHERE type = '#{t}'"
+    end
+    
+
     add_column :account_entries, :creditor, :boolean, :null => false, :default => 0
     add_column :account_entries, :line_number, :integer, :null => false, :default => 0
 
