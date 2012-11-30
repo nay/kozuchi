d08fe6d9605ee8dae65c443479f6120c079c371a
nay <y.ohba@everyleaf.com>
2012-11-30 15:15:18 +0900

    [#40094691] Implement Ajax post to DealPatternsController#create.

--- app/controllers/deal_patterns_controller.rb
+++ app/controllers/deal_patterns_controller.rb
@@ -4,9 +4,11 @@ class DealPatternsController < ApplicationController
   # 仕訳帳・口座別出納などの記入フォームからの、Ajax による簡易なパターン登録
   # 最近のパターン10件エリアの表示内容を返す
   def create
-    deal_pattern = Pattern::Deal.new(params[:deal])
-    deal_pattern.save!
-    # TODO: エラー処理
+    p params[:deal].inspect
+
+#    deal_pattern = Pattern::Deal.new(params[:deal])
+#    deal_pattern.save!
+#    # TODO: エラー処理
 
     @deal_patterns = Pattern::Deal.order('updated_at desc').limit(10)
 
