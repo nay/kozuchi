b30608851f13543bd589da844f22f0e64d18dcc7
nay3 <y.ohba@everyleaf.com>
2012-12-02 09:44:49 +0900

    [#40094691] Reflect the result of adding a new deal_pattern from complex deal form.

--- app/controllers/deal_patterns_controller.rb
+++ app/controllers/deal_patterns_controller.rb
@@ -4,13 +4,10 @@ class DealPatternsController < ApplicationController
   # 仕訳帳・口座別出納などの記入フォームからの、Ajax による簡易なパターン登録
   # 最近のパターン10件エリアの表示内容を返す
   def create
-    deal_pattern = Pattern::Deal.new(params[:deal])
-    deal_pattern.user_id = current_user.id
-    deal_pattern.save!
+    current_user.deal_patterns.create!(params[:deal])
+    # TODO: 通常利用ではあまり発生しないはずだが、エラー時の処理
 
-    @deal_patterns = Pattern::Deal.order('updated_at desc').limit(10)
-
-    render :partial => 'new_arrivals'
+    render :partial => '/shared/deal_patterns/recent'
   end
 
 end
