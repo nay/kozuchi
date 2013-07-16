f30b4782fda73faa60db13bcaf39b1baa0961b60
nay3 <y.ohba@everyleaf.com>
2013-07-16 14:02:52 +0900

    Update selenium-webdriver. Retrieve utility methods from general_spec.rb

--- spec/models/deal/general_spec.rb
+++ spec/models/deal/general_spec.rb
@@ -314,43 +314,4 @@ describe Deal::General do
     end
   end
 
-  def to_account_id(value)
-    case value
-    when Symbol
-      Fixtures.identify(value)
-    when ActiveRecord::Base
-      value.id
-    else
-      value
-    end
-  end
-
-  def new_deal(month, day, from, to, amount, year = 2008)
-    d = Deal::General.new(:summary => "#{month}/#{day}の買い物",
-      :debtor_entries_attributes => [:account_id => to_account_id(to), :amount => amount],
-      :creditor_entries_attributes => [:account_id => to_account_id(from), :amount => amount * -1],
-      :date => Date.new(year, month, day))
-    to_account = Account::Base.find(to_account_id(to))
-    d.user_id = to_account.user_id
-    d
-  end
-
-  # debtors {account_id => amout, account_id => amount} のように記述
-  def new_complex_deal(month, day, debtors, creditors, options = {})
-    summary = options[:summary] || "#{month}/#{day}の記入"
-    date = Date.new(options[:year] || 2010, month, day)
-
-    deal = Deal::General.new(:summary => summary, :summary_mode => 'unify', :date => date,
-      :debtor_entries_attributes => debtors.map{|key, value| {:account_id => (key.kind_of?(Symbol) ? Fixtures.identify(key) : key), :amount => value}}.each_with_index{|e, i| e[:line_number] = i},
-      :creditor_entries_attributes => creditors.map{|key, value| {:account_id => (key.kind_of?(Symbol) ? Fixtures.identify(key) : key), :amount => value}}.each_with_index{|e, i| e[:line_number] = i}
-    )
-    
-    key = debtors.keys.first
-    account_id = key.kind_of?(Symbol) ? Fixtures.identify(key) : key
-    account = Account::Base.find_by_id(account_id)
-    raise "no account" unless account
-    deal.user_id = account.user_id
-    deal
-  end
-
 end
---
+++ spec/support/deal_util.rb
@@ -0,0 +1,44 @@
+# -*- encoding : utf-8 -*-
+# Deal 関係のユーティリティメソッド
+
+# 口座idを取得するためのユーティリティ fixture名またはレコードを渡す
+def to_account_id(value)
+  case value
+  when Symbol
+    Fixtures.identify(value)
+  when ActiveRecord::Base
+    value.id
+  else
+    value
+  end
+end
+
+# 単純明細記入の作成
+def new_deal(month, day, from, to, amount, year = 2008)
+  d = Deal::General.new(:summary => "#{month}/#{day}の買い物",
+    :debtor_entries_attributes => [:account_id => to_account_id(to), :amount => amount],
+    :creditor_entries_attributes => [:account_id => to_account_id(from), :amount => amount * -1],
+    :date => Date.new(year, month, day))
+  to_account = Account::Base.find(to_account_id(to))
+  d.user_id = to_account.user_id
+  d
+end
+
+# 複数明細記入の作成
+# debtors {account_id => amout, account_id => amount} のように記述
+def new_complex_deal(month, day, debtors, creditors, options = {})
+  summary = options[:summary] || "#{month}/#{day}の記入"
+  date = Date.new(options[:year] || 2010, month, day)
+
+  deal = Deal::General.new(:summary => summary, :summary_mode => 'unify', :date => date,
+    :debtor_entries_attributes => debtors.map{|key, value| {:account_id => (key.kind_of?(Symbol) ? Fixtures.identify(key) : key), :amount => value}}.each_with_index{|e, i| e[:line_number] = i},
+    :creditor_entries_attributes => creditors.map{|key, value| {:account_id => (key.kind_of?(Symbol) ? Fixtures.identify(key) : key), :amount => value}}.each_with_index{|e, i| e[:line_number] = i}
+  )
+
+  key = debtors.keys.first
+  account_id = key.kind_of?(Symbol) ? Fixtures.identify(key) : key
+  account = Account::Base.find_by_id(account_id)
+  raise "no account" unless account
+  deal.user_id = account.user_id
+  deal
+end
