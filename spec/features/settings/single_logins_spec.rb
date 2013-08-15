c37db078fc02ef8e55d1c19907e2ee72e70af4f0
nay3 <y.ohba@everyleaf.com>
2013-08-15 16:02:54 +0900

    [#55211808] Fix single_logins_controller#create not to use dirty relation object.

--- spec/features/settings/single_logins_spec.rb
+++ spec/features/settings/single_logins_spec.rb
@@ -13,5 +13,30 @@ describe Settings::SingleLoginsController do
     it "ログイン名入力欄がある" do
       page.should have_css('input#single_login_login')
     end
+
+    describe "新しい設定の登録" do
+      before do
+        fill_in 'single_login_login', :with => login
+        fill_in 'single_login_password', :with => password
+        click_button '新しい設定を登録'
+      end
+      context "パスワードが正しいとき" do
+        let(:login) { 'hanako' }
+        let(:password) { 'hanako' }
+        it "追加メッセージが出て、ログイン名入力欄がある" do
+          flash_notice.should have_content('追加しました')
+          page.should have_css('input#single_login_login')
+        end
+      end
+      context "パスワードが異なるとき" do
+        let(:login) { 'hanako' }
+        let(:password) { 'wrong' }
+        it "追加メッセージ欄がなく、ログイン名入力欄がある" do
+          page.should_not have_css('#notice')
+          page.should have_css('input#single_login_login')
+        end
+      end
+    end
   end
+
 end
