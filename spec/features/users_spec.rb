3a33daad52797b546968dfdf3fa9fd37ead75362
nay3 <y.ohba@everyleaf.com>
2014-02-24 11:37:58 +0900

    [#64961508] Add specs for password change.

--- spec/features/users_spec.rb
+++ spec/features/users_spec.rb
@@ -14,8 +14,9 @@ describe UsersController do
   end
 
   describe "ユーザー登録できる" do
-    context "when not logged in" do
+    context "メール送信をスキップする設定のとき" do
       before do
+        SKIP_MAIL = true # 警告が出るがしかたない。TODO: 変更可能な形態にする
         visit "/"
         click_link "ユーザー登録"
         fill_in "ログインID", with: "featuretest"
@@ -29,8 +30,58 @@ describe UsersController do
         expect(flash_notice).to have_content "登録が完了しました。"
         expect(page.current_path).to eq "/home"
       end
+    end
 
+    context "メール送信ありの設定のとき" do
+      before do
+        SKIP_MAIL = false # 警告が出るがしかたない。TODO: 変更可能な形態にする
+        visit "/"
+        click_link "ユーザー登録"
+        fill_in "ログインID", with: "featuretest"
+        fill_in "Email", with: "featuretest@kozuchi.net"
+        fill_in "パスワード", with: "testtest"
+        fill_in "パスワード（確認）", with: "testtest"
+        click_button "ユーザー登録"
+      end
 
+      it do
+        expect(page).to have_content "ご登録ありがとうございます。確認メールが送信されますので、記載されているURLからアカウントを有効にしてください。"
+      end
+    end
+  end
+
+  describe "パスワードを忘れたとき" do
+    context "メール送信をスキップする設定のとき" do
+      before do
+        SKIP_MAIL = true # 警告が出るがしかたない。TODO: 変更可能な形態にする
+        visit "/"
+        click_link "パスワードを忘れたとき"
+      end
+      it { expect(page).to have_content "現在、システムからメールを送信しない設定となっているため、Webからパスワード再設定をしていただくことができません。" }
+    end
+
+    context "メール送信ありの設定のとき" do
+      before do
+        SKIP_MAIL = false # 警告が出るがしかたない。TODO: 変更可能な形態にする
+        visit "/"
+        click_link "パスワードを忘れたとき"
+      end
+      it { expect(page).to have_css("input#email") }
+
+      describe "パスワード変更の実行" do
+        before do
+          fill_in :email, with: email
+          click_button "パスワード変更を希望する"
+        end
+        context "登録されているメールアドレスのとき" do
+          let(:email) { "taro@kozuchi.net" }
+          it { expect(page).to have_content "パスワード変更のための情報を #{email} へ送信しました。"}
+        end
+        context "登録されていないメールアドレスのとき" do
+          let(:email) { "unknown@kozuchi.net" }
+          it { expect(page).to have_content "該当するユーザーは登録されていません。" }
+        end
+      end
     end
   end
 end
