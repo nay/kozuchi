007dd54fbd4c6bc65e207cfa2bff24b934cf07fe
nay3 <y.ohba@everyleaf.com>
2014-05-02 07:27:00 +0900

    Localize logout message.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -62,7 +62,7 @@ class SessionsController < ApplicationController
     self.current_user.forget_me if logged_in?
     cookies.delete :auth_token
     reset_session
-    flash[:notice] = "You have been logged out."
+    flash[:notice] = "ログアウトしました。"
     redirect_back_or_default(root_path)
   end
 
