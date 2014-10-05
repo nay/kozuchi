423b345b7e595519844f9203855886069991a5d1
nay3 <y.ohba@everyleaf.com>
2014-10-06 02:01:36 +0900

    Use test utility methods

--- spec/features/profit_and_loss_js_spec.rb
+++ spec/features/profit_and_loss_js_spec.rb
@@ -11,13 +11,12 @@ describe ProfitAndLossController, :js => true do
   let(:target_date) {Date.today >> 1}
   before do
     Deal::Base.destroy_all
-    find('#header_menu').click_link '家計簿'
-    click_link '収支表'
+    select_menu('分析', '収支表')
   end
 
   describe "カレンダー（翌月）のクリック" do
     before do
-      find("#month_#{target_date.year}_#{target_date.month}").click_link("#{target_date.month}月")
+      click_calendar(target_date.year, target_date.month)
     end
     it do
       page.should have_content("#{target_date.year}年#{target_date.month}月末日の収支表")
