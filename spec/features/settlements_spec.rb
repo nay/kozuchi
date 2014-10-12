1303edbf0ba4d41840d5a6213c60b759b702f3cf
nay3 <y.ohba@everyleaf.com>
2014-10-13 03:18:40 +0900

    Fix settlements_spec.rb

--- spec/features/settlements_spec.rb
+++ spec/features/settlements_spec.rb
@@ -7,14 +7,15 @@ describe SettlementsController do
 
   include_context "太郎 logged in"
 
-  describe "新しい精算" do
+  describe "精算一覧" do
+    let(:url) { '/settlements' }
 
     context "資産口座が現金しかないとき" do
       before do
         AccountLink.destroy_all
         AccountLinkRequest.destroy_all
         Account::Asset.destroy_all("asset_kind != 'cache'")
-        visit '/settlements/new'
+        visit url
       end
 
       it "精算機能が利用できないと表示される" do
@@ -23,48 +24,13 @@ describe SettlementsController do
     end
 
     context "資産口座にカードがあるとき" do
-
-      context "対象記入がないとき" do
-        before do
-          visit '/settlements/new'
-        end
-        it "口座や期間のフォームがあり、データがない旨が表示される" do
-          page.should have_css("select#settlement_account_id")
-          page.should have_css("select#start_date_year")
-          page.should have_css("select#end_date_day")
-          page.should have_css("div#target_deals")
-          page.should have_content("の未精算取引 は 全0件あります。")
-        end
+      # TODO: 精算データの有無で分けたい
+      before do
+        visit url
       end
-
-      context "対象記入があるとき" do
-        let!(:target_asset) {current_user.assets.credit.first}
-        let!(:target_date) {
-          prev_month = Date.today << 1
-          Date.new(prev_month.year, prev_month.month, 5) # 先月5日
-        }
-
-        before do
-          FactoryGirl.create(:general_deal,
-            :date => target_date,
-            :summary => 'ランチのパスタ',
-            :creditor_entries_attributes => [:account_id => target_asset.id, :amount => -800])
-          visit '/settlements/new'
-        end
-
-        it "口座や期間のフォームがあり、データが表示される" do
-          page.should have_css("select#settlement_account_id")
-          page.should have_css("select#start_date_year")
-          page.should have_css("select#end_date_day")
-          page.should have_css("div#target_deals")
-          page.should have_content("の未精算取引 は 全1件あります。")
-          page.should have_css("table.book")
-          page.should have_content("ランチのパスタ")
-          page.should have_content("800")
-        end
+      it "セルがある" do
+        page.should have_css("td.settlement")
       end
     end
-
   end
-
 end
