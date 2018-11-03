9953118b65db7c7c8777bb6011c4ac4fa4b51aad
kndoo <kndoo255@gmail.com>
2018-11-03 16:52:31 +0900

    Add spec for settlements summary

--- spec/features/settlements_spec.rb
+++ spec/features/settlements_spec.rb
@@ -14,5 +14,20 @@ describe "Settlements Features", type: :feature do
     end
 
     it { expect(menu_bar).to eq "すべての精算" }
+    it { expect(page.find('.book.settlements_summary')).to have_content "#{Time.zone.today.year}年" }
+    it { expect(page.find('.book.settlements_summary')).to have_content "#{Time.zone.today.month}月" }
+  end
+
+  describe "精算（口座指定）" do
+    let(:account) { accounts(:taro_card) }
+    let(:url) { "/accounts/#{account.id}/settlements/#{Time.zone.today.year}/#{Time.zone.today.month}" }
+
+    before do
+      visit url
+    end
+
+    it { expect(menu_bar).to eq "#{account.name}の精算" }
+    it { expect(page.find('.book.settlements_summary')).to have_content "#{Time.zone.today.year}年" }
+    it { expect(page.find('.book.settlements_summary')).to have_content "#{Time.zone.today.month}月" }
   end
 end
