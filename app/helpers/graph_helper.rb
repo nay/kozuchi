286399e09dfd1f11fe5cea9feb8ca09d69a01100
kndoo <kndoo255@gmail.com>
2018-11-03 22:49:41 +0900

    Fix misaligned text in pie chart

--- app/helpers/graph_helper.rb
+++ app/helpers/graph_helper.rb
@@ -39,7 +39,7 @@ $ (function() {
 	cg.draw(items);
 })
     EOS
-    s << %Q(<div><canvas width="400" height="200" id="#{id}"></canvas></div>).html_safe
+    s << %Q(<div style="margin: 0 auto; width: 400px;"><canvas width="400" height="200" id="#{id}"></canvas></div>).html_safe
     s
   end
   
