71108029bceb0fcd1df3b669d9c5b126f5a97f31
nay <y.ohba@everyleaf.com>
2018-11-12 20:18:03 +0900

    Use expect instead of should in spec/models/deal/general_spec.rb

--- spec/models/deal/general_spec.rb
+++ spec/models/deal/general_spec.rb
@@ -16,7 +16,7 @@ describe Deal::General do
       let!(:initial_balance) { FactoryGirl.create(:balance_deal, :date => Date.new(2012, 12, 5), :balance => 1000)}
 
       it "現金の初回残高記入のamountは1000である" do
-        initial_balance.amount.should == 1000
+        expect(initial_balance.amount).to eq 1000
       end
 
       context "未確定記入(現金からの支出800円)を確定したとき" do
@@ -25,7 +25,7 @@ describe Deal::General do
           initial_balance.reload
         end
         it "初回残高記入のamountは1800である" do
-          initial_balance.amount.should == 1800
+          expect(initial_balance.amount).to eq 1800
         end
       end
     end
@@ -51,37 +51,37 @@ describe Deal::General do
           }
         ]
       )
-      deal.debtor_entries.size.should == 1
-      deal.creditor_entries.size.should == 2
+      expect(deal.debtor_entries.size).to eq 1
+      expect(deal.creditor_entries.size).to eq 2
     end
   end
 
   describe "valid?" do
     it "数字の合った複合Dealが検証をとおること" do
       deal = new_complex_deal(3, 1, {:taro_food => 1300},{:taro_cache => -1000, :taro_bank => -300})
-      deal.valid?.should be_truthy
+      expect(deal.valid?).to be_truthy
     end
     it "数字の合わない複合Dealが検証を通らないこと" do
       deal = new_complex_deal(3, 1, {:taro_food => 1300},{:taro_cache => -1000, :taro_bank => -200})
-      deal.valid?.should be_falsey
+      expect(deal.valid?).to be_falsey
     end
     it "amountが0のEntryを含む複合Dealが検証を通らないこと" do
       deal = new_complex_deal(3, 1, {:taro_food => 1300},{:taro_cache => -1300, :taro_bank => 0})
-      deal.valid?.should be_falsey
+      expect(deal.valid?).to be_falsey
     end
     it "複合Dealの各Entryの口座に重複があっても構わない" do
       deal = new_complex_deal(3, 1, {:taro_food => 1400}, {:taro_cache => -1300, :taro_food => -100})
-      deal.valid?.should be_truthy
+      expect(deal.valid?).to be_truthy
     end
 
     it "同じ口座間での移動が検証を通る" do
       deal = new_simple_deal(3, 1, :taro_food, :taro_food, 300)
-      deal.valid?.should be_truthy
+      expect(deal.valid?).to be_truthy
     end
     it "金額が0ではいけないこと" do
       deal = new_simple_deal(3, 1, :taro_food, :taro_cache, 0)
       deal.valid?
-      deal.valid?.should be_falsey
+      expect(deal.valid?).to be_falsey
     end
   end
 
@@ -89,57 +89,57 @@ describe Deal::General do
 
     it "数字の合った複合Dealが作成できること" do
       deal = new_complex_deal(3, 1, {:taro_food => 1300},{:taro_cache => -1000, :taro_bank => -300})
-      deal.save.should be_truthy
+      expect(deal.save).to be_truthy
     end
     it "カンマ入り数字による複合Dealが作成できること" do
       deal = new_complex_deal(3, 1, {:taro_food => '1,300'},{:taro_cache => '-1,000', :taro_bank => '-300'})
-      deal.save.should be_truthy
-      deal.debtor_entries.any?{|e| e.amount.to_i == 1300}.should be_truthy
+      expect(deal.save).to be_truthy
+      expect(deal.debtor_entries.any?{|e| e.amount.to_i == 1300}).to be_truthy
     end
     it "複合Dealの各Entryの口座に重複があっても作成できること" do
       deal = new_complex_deal(3, 1, {:taro_food => 1400}, {:taro_cache => -1300, :taro_food => -100})
-      deal.save.should be_truthy
+      expect(deal.save).to be_truthy
 
       d = deal.debtor_entries.first
-      d.account_id.should == Fixtures.identify(:taro_food)
-      d.amount.should == 1400
+      expect(d.account_id).to eq Fixtures.identify(:taro_food)
+      expect(d.amount).to eq 1400
 
       c = deal.creditor_entries.first
-      c.account_id.should == Fixtures.identify(:taro_cache)
-      c.amount.should == -1300
+      expect(c.account_id).to eq Fixtures.identify(:taro_cache)
+      expect(c.amount).to eq -1300
 
       c = deal.creditor_entries.last
-      c.account_id.should == Fixtures.identify(:taro_food)
-      c.amount.should == -100
+      expect(c.account_id).to eq Fixtures.identify(:taro_food)
+      expect(c.amount).to eq -100
     end
     it "片側に空白欄のある複合Dealが作成でき、空白欄の分が維持される" do
       deal = FactoryGirl.build(:complex_deal,
         :debtor_entries_attributes =>  [{:account_id => Fixtures.identify(:taro_food), :amount => 800, :line_number => 0}, {:account_id => Fixtures.identify(:taro_other), :amount => 200, :line_number => 1}],
         :creditor_entries_attributes => [:account_id => Fixtures.identify(:taro_cache), :amount => -1000, :line_number => 1] # 0でない
       )
-      deal.save.should be_truthy
-      deal.creditor_entries.first.line_number.should == 1
+      expect(deal.save).to be_truthy
+      expect(deal.creditor_entries.first.line_number).to eq 1
     end
     it "両側に空白欄のある複合Dealが作成でき、空白分が詰められる" do
       deal = FactoryGirl.build(:complex_deal,
         :debtor_entries_attributes =>  [{:account_id => Fixtures.identify(:taro_food), :amount => 800, :line_number => 0}, {:account_id => Fixtures.identify(:taro_other), :amount => 200, :line_number => 2}],
         :creditor_entries_attributes => [:account_id => Fixtures.identify(:taro_cache), :amount => -1000, :line_number => 2] # 0, 1でない
       )
-      deal.save.should be_truthy
-      deal.debtor_entries.map(&:line_number).should == [0, 1]
-      deal.creditor_entries.first.line_number.should == 1
+      expect(deal.save).to be_truthy
+      expect(deal.debtor_entries.map(&:line_number)).to eq [0, 1]
+      expect(deal.creditor_entries.first.line_number).to eq 1
     end
 
     it "同じ口座間での移動記入が作成できること" do
       deal = new_simple_deal(3, 1, :taro_food, :taro_food, 300)
-      deal.save.should be_truthy
+      expect(deal.save).to be_truthy
       d = deal.debtor_entries.first
-      d.account_id.should == Fixtures.identify(:taro_food)
-      d.amount.should == 300
+      expect(d.account_id).to eq Fixtures.identify(:taro_food)
+      expect(d.amount).to eq 300
 
       c = deal.creditor_entries.first
-      c.account_id.should == Fixtures.identify(:taro_food)
-      c.amount.should == -300
+      expect(c.account_id).to eq Fixtures.identify(:taro_food)
+      expect(c.amount).to eq -300
     end
 
     describe "連携なし" do
@@ -148,12 +148,12 @@ describe Deal::General do
       end
 
       it "成功する" do
-        @deal.save.should be_truthy
+        expect(@deal.save).to be_truthy
       end
 
       it "user_id, date, daily_seqがentriesに引き継がれる" do
         @deal.save!
-        @deal.entries.detect{|e| e.user_id != @deal.user_id || e.date != @deal.date || e.daily_seq != @deal.daily_seq}.should be_nil
+        expect(@deal.entries.detect{|e| e.user_id != @deal.user_id || e.date != @deal.date || e.daily_seq != @deal.daily_seq}).to be_nil
       end
 
       it "account_entryを手動で足してもcreateできる" do
@@ -166,9 +166,9 @@ describe Deal::General do
         deal.debtor_entries.build(
           :account_id => @bank.id,
           :amount => 10000)
-        deal.save.should be_truthy
-        deal.creditor_entries.detect{|e| e.new_record?}.should be_nil
-        deal.debtor_entries.detect{|e| e.new_record?}.should be_nil
+        expect(deal.save).to be_truthy
+        expect(deal.creditor_entries.detect{|e| e.new_record?}).to be_nil
+        expect(deal.debtor_entries.detect{|e| e.new_record?}).to be_nil
       end
     end
 
@@ -198,17 +198,17 @@ describe Deal::General do
           '0' => {:account_id => @bank.id, :amount => 3500, :id => @deal.debtor_entries.first.id, :line_number => 0}
         }
       }
-      @deal.creditor_entries.size.should == 3 # 一時的に３つになる
-      @deal.creditor_entries.first.marked_for_destruction?.should be_truthy
-      @deal.creditor_entries[1].amount.should == -3200
-      @deal.creditor_entries[2].amount.should == -300
-      @deal.valid?.should be_truthy
-      @deal.save.should be_truthy
+      expect(@deal.creditor_entries.size).to eq 3 # 一時的に３つになる
+      expect(@deal.creditor_entries.first.marked_for_destruction?).to be_truthy
+      expect(@deal.creditor_entries[1].amount).to eq -3200
+      expect(@deal.creditor_entries[2].amount).to eq -300
+      expect(@deal.valid?).to be_truthy
+      expect(@deal.save).to be_truthy
       @deal.reload
-      @deal.creditor_entries.size.should == 2
-      @deal.debtor_entries.size.should == 1
+      expect(@deal.creditor_entries.size).to eq 2
+      expect(@deal.debtor_entries.size).to eq 1
       # 古いentryがnullifyでのこっていないこと
-      Entry::Base.find_by(deal_id: nil).should be_nil
+      expect(Entry::Base.find_by(deal_id: nil)).to be_nil
     end
     it "借り方の項目を足して複数記入に変更できる" do
       @deal.attributes = {
@@ -220,17 +220,17 @@ describe Deal::General do
           '1' => {:account_id => :deal_test_food.to_id, :amount => 300, :line_number => 1}
         }
       }
-      @deal.debtor_entries.size.should == 3 # 一時的に３つになる
-      @deal.debtor_entries.first.marked_for_destruction?.should be_truthy
-      @deal.debtor_entries[1].amount.should == 3200
-      @deal.debtor_entries[2].amount.should == 300
-      @deal.valid?.should be_truthy
-      @deal.save.should be_truthy
+      expect(@deal.debtor_entries.size).to eq 3 # 一時的に３つになる
+      expect(@deal.debtor_entries.first.marked_for_destruction?).to be_truthy
+      expect(@deal.debtor_entries[1].amount).to eq 3200
+      expect(@deal.debtor_entries[2].amount).to eq 300
+      expect(@deal.valid?).to be_truthy
+      expect(@deal.save).to be_truthy
       @deal.reload
-      @deal.creditor_entries.size.should == 1
-      @deal.debtor_entries.size.should == 2
+      expect(@deal.creditor_entries.size).to eq 1
+      expect(@deal.debtor_entries.size).to eq 2
       # 古いentryがnullifyでのこっていないこと
-      Entry::Base.find_by(deal_id: nil).should be_nil
+      expect(Entry::Base.find_by(deal_id: nil)).to be_nil
     end
     context "日付を変更したとき" do
       before do
@@ -239,11 +239,11 @@ describe Deal::General do
         deal.save!
       end
       it "entriesのdateも変更される" do
-        deal.entries.detect{|e| e.user_id != deal.user_id || e.date != deal.date || e.daily_seq != deal.daily_seq}.should be_nil
+        expect(deal.entries.detect{|e| e.user_id != deal.user_id || e.date != deal.date || e.daily_seq != deal.daily_seq}).to be_nil
       end
       it "dealのcreated_atは変化せず、updated_atは更新される" do
-        deal.created_at.to_s.should == old_time_stamp.to_s
-        deal.updated_at.to_s.should_not == old_time_stamp.to_s
+        expect(deal.created_at.to_s).to eq old_time_stamp.to_s
+        expect(deal.updated_at.to_s).not_to eq old_time_stamp.to_s
       end
     end
     context "摘要を変更したとき" do
@@ -252,8 +252,8 @@ describe Deal::General do
         deal.save!
       end
       it "dealのcreated_atは変化せず、updated_atは更新される" do
-        deal.created_at.to_s.should == old_time_stamp.to_s
-        deal.updated_at.to_s.should_not == old_time_stamp.to_s
+        expect(deal.created_at.to_s).to eq old_time_stamp.to_s
+        expect(deal.updated_at.to_s).not_to eq old_time_stamp.to_s
       end
     end
 
@@ -267,19 +267,19 @@ describe Deal::General do
       @deal.save!
 
       @deal.reload
-      @deal.debtor_entries.size.should == 1
+      expect(@deal.debtor_entries.size).to eq 1
       new_debtor_entry = @deal.debtor_entries.first
-      new_debtor_entry.id.should == old_debtor_entry.id
-      new_debtor_entry.amount.should == old_debtor_entry.amount
-      new_debtor_entry.account_id.should == old_debtor_entry.account_id
-      new_debtor_entry.user_id.should == old_debtor_entry.user_id
+      expect(new_debtor_entry.id).to eq old_debtor_entry.id
+      expect(new_debtor_entry.amount).to eq old_debtor_entry.amount
+      expect(new_debtor_entry.account_id).to eq old_debtor_entry.account_id
+      expect(new_debtor_entry.user_id).to eq old_debtor_entry.user_id
 
-      @deal.creditor_entries.size.should == 1
+      expect(@deal.creditor_entries.size).to eq 1
       new_creditor_entry = @deal.creditor_entries.first
-      new_creditor_entry.id.should == old_creditor_entry.id
-      new_creditor_entry.amount.should == old_creditor_entry.amount
-      new_creditor_entry.account_id.should == old_creditor_entry.account_id
-      new_creditor_entry.user_id.should == old_creditor_entry.user_id
+      expect(new_creditor_entry.id).to eq old_creditor_entry.id
+      expect(new_creditor_entry.amount).to eq old_creditor_entry.amount
+      expect(new_creditor_entry.account_id).to eq old_creditor_entry.account_id
+      expect(new_creditor_entry.user_id).to eq old_creditor_entry.user_id
     end
   
     context "with a complex deal" do
@@ -296,9 +296,9 @@ describe Deal::General do
         expect{ deal.save! }.not_to raise_error
         deal.reload
 
-        deal.debtor_entries.first.id.should_not == entry.id
-        deal.debtor_entries.first.account_id.should == entry.account_id
-        deal.debtor_entries.first.line_number.should == entry.line_number
+        expect(deal.debtor_entries.first.id).not_to eq entry.id
+        expect(deal.debtor_entries.first.account_id).to eq entry.account_id
+        expect(deal.debtor_entries.first.line_number).to eq entry.line_number
       end
     end
 
@@ -324,26 +324,26 @@ describe Deal::General do
         expect{ deal.save! }.not_to raise_error
 
         deal.reload
-        deal.debtor_entries.size.should == 2
+        expect(deal.debtor_entries.size).to eq 2
         new_debtor_entries = deal.debtor_entries.to_a
-        new_debtor_entries[0].id.should == old_debtor_entries[0].id
-        new_debtor_entries[0].amount.should == old_debtor_entries[0].amount
-        new_debtor_entries[0].account_id.should == old_debtor_entries[0].account_id
-        new_debtor_entries[0].user_id.should == old_debtor_entries[0].user_id
-        new_debtor_entries[0].line_number.should == old_debtor_entries[0].line_number
-        new_debtor_entries[1].id.should == old_debtor_entries[1].id
-        new_debtor_entries[1].amount.should == old_debtor_entries[1].amount
-        new_debtor_entries[1].account_id.should == old_debtor_entries[1].account_id
-        new_debtor_entries[1].user_id.should == old_debtor_entries[1].user_id
-        new_debtor_entries[1].line_number.should == old_debtor_entries[1].line_number
+        expect(new_debtor_entries[0].id).to eq old_debtor_entries[0].id
+        expect(new_debtor_entries[0].amount).to eq old_debtor_entries[0].amount
+        expect(new_debtor_entries[0].account_id).to eq old_debtor_entries[0].account_id
+        expect(new_debtor_entries[0].user_id).to eq old_debtor_entries[0].user_id
+        expect(new_debtor_entries[0].line_number).to eq old_debtor_entries[0].line_number
+        expect(new_debtor_entries[1].id).to eq old_debtor_entries[1].id
+        expect(new_debtor_entries[1].amount).to eq old_debtor_entries[1].amount
+        expect(new_debtor_entries[1].account_id).to eq old_debtor_entries[1].account_id
+        expect(new_debtor_entries[1].user_id).to eq old_debtor_entries[1].user_id
+        expect(new_debtor_entries[1].line_number).to eq old_debtor_entries[1].line_number
 
-        deal.creditor_entries.size.should == 1
+        expect(deal.creditor_entries.size).to eq 1
         new_creditor_entry = deal.creditor_entries.first
-        new_creditor_entry.id.should == old_creditor_entry.id
-        new_creditor_entry.amount.should == old_creditor_entry.amount
-        new_creditor_entry.account_id.should == old_creditor_entry.account_id
-        new_creditor_entry.user_id.should == old_creditor_entry.user_id
-        new_creditor_entry.line_number.should == old_creditor_entry.line_number
+        expect(new_creditor_entry.id).to eq old_creditor_entry.id
+        expect(new_creditor_entry.amount).to eq old_creditor_entry.amount
+        expect(new_creditor_entry.account_id).to eq old_creditor_entry.account_id
+        expect(new_creditor_entry.user_id).to eq old_creditor_entry.user_id
+        expect(new_creditor_entry.line_number).to eq old_creditor_entry.line_number
       end
     end
 
