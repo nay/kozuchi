ed819380ffda8b272c1a89d53076db10b73c757d
nay <y.ohba@everyleaf.com>
2018-11-14 06:04:22 +0900

    Use expect instead of should in spec/models/pattern/deal_spec.rb

--- spec/models/pattern/deal_spec.rb
+++ spec/models/pattern/deal_spec.rb
@@ -15,28 +15,28 @@ describe Pattern::Deal do
       let(:deal_pattern) { FactoryGirl.create(:deal_pattern, :code => 'SALARY', :name => '給与') }
       subject {deal_pattern.to_s}
 
-      it { should == 'SALARY 給与'}
+      it { is_expected.to eq 'SALARY 給与'}
     end
 
     context "with name and without code" do
       let(:deal_pattern) { FactoryGirl.create(:deal_pattern, :code => nil, :name => '給与') }
       subject {deal_pattern.to_s}
 
-      it { should == '給与'}
+      it { is_expected.to eq '給与'}
     end
 
     context "with code and without name" do
       let(:deal_pattern) { FactoryGirl.create(:deal_pattern, :code => 'SALARY', :name => '') }
       subject {deal_pattern.to_s}
 
-      it { should == 'SALARY *給料'}
+      it { is_expected.to eq 'SALARY *給料'}
     end
 
     context "without code and name" do
       let(:deal_pattern) { FactoryGirl.create(:deal_pattern, :code => nil, :name => '') }
       subject {deal_pattern.to_s}
 
-      it { should == '*給料'}
+      it { is_expected.to eq '*給料'}
     end
   end
 
@@ -44,28 +44,28 @@ describe Pattern::Deal do
     let(:deal_pattern) { FactoryGirl.build(:deal_pattern, :overwrites_code => '1') }
     subject{deal_pattern.assignable_attributes.keys}
     it "column のほか、summary_mode と summaryも含まれる" do
-      should be_include('name') # column代表で
+      is_expected.to be_include('name') # column代表で
 
-      should be_include('summary_mode')
-      should be_include('summary')
+      is_expected.to be_include('summary_mode')
+      is_expected.to be_include('summary')
 
-      should be_include('debtor_entries_attributes')
-      should be_include('creditor_entries_attributes')
+      is_expected.to be_include('debtor_entries_attributes')
+      is_expected.to be_include('creditor_entries_attributes')
     end
   end
 
   describe "#overwrites_code?" do
     context "overwrites_code が '1' のとき" do
       let(:deal_pattern) { FactoryGirl.build(:deal_pattern, :overwrites_code => '1') }
-      it { deal_pattern.overwrites_code?.should be_truthy }
+      it { expect(deal_pattern.overwrites_code?).to be_truthy }
     end
     context "overwrites_code が nil のとき" do
       let(:deal_pattern) { FactoryGirl.build(:deal_pattern, :overwrites_code => nil) }
-      it { deal_pattern.overwrites_code?.should be_falsey }
+      it { expect(deal_pattern.overwrites_code?).to be_falsey }
     end
     context "overwrites_code が '0' のとき" do
       let(:deal_pattern) { FactoryGirl.build(:deal_pattern, :overwrites_code => '0') }
-      it { deal_pattern.overwrites_code?.should be_falsey }
+      it { expect(deal_pattern.overwrites_code?).to be_falsey }
     end
   end
 
@@ -73,16 +73,16 @@ describe Pattern::Deal do
     context "空のコード" do
       let(:deal_pattern) { FactoryGirl.build(:deal_pattern, :code => '', :name => 'TEST PATTERN 2') }
       it "空のコードを保存するとコードはNULLになる" do
-        deal_pattern.save.should be_truthy
-        deal_pattern.code.should be_nil
+        expect(deal_pattern.save).to be_truthy
+        expect(deal_pattern.code).to be_nil
         deal_pattern.reload
-        deal_pattern.code.should be_nil
+        expect(deal_pattern.code).to be_nil
       end
       it "検証なしで空のコードを保存してもコードはNULLになる" do
-        deal_pattern.save(:validate => false).should be_truthy
-        deal_pattern.code.should be_nil
+        expect(deal_pattern.save(:validate => false)).to be_truthy
+        expect(deal_pattern.code).to be_nil
         deal_pattern.reload
-        deal_pattern.code.should be_nil
+        expect(deal_pattern.code).to be_nil
       end
     end
     describe "コードの重複" do
@@ -90,20 +90,20 @@ describe Pattern::Deal do
       context "大文字小文字の異なる重複コードが登録されており、overwrites_codeが指定されていないとき" do
         let(:deal_pattern) { FactoryGirl.build(:deal_pattern, :code => 'codeX', :name => 'TEST PATTERN 2') }
         it do
-          deal_pattern.save.should be_truthy
+          expect(deal_pattern.save).to be_truthy
         end
       end
       context "重複コードが登録されており、overwrites_codeが指定されているとき" do
         let(:deal_pattern) { FactoryGirl.build(:deal_pattern, :code => 'CODEX', :name => 'TEST PATTERN 2', :overwrites_code => '1') }
         it "saveできる" do
-          deal_pattern.save.should be_truthy
-          deal_pattern.id.should == existing.id
-          deal_pattern.debtor_entries.size.should == 2
-          deal_pattern.creditor_entries.size.should == 1
+          expect(deal_pattern.save).to be_truthy
+          expect(deal_pattern.id).to eq existing.id
+          expect(deal_pattern.debtor_entries.size).to eq 2
+          expect(deal_pattern.creditor_entries.size).to eq 1
           deal_pattern.reload
-          deal_pattern.id.should == existing.id
-          deal_pattern.debtor_entries.size.should == 2
-          deal_pattern.creditor_entries.size.should == 1
+          expect(deal_pattern.id).to eq existing.id
+          expect(deal_pattern.debtor_entries.size).to eq 2
+          expect(deal_pattern.creditor_entries.size).to eq 1
         end
       end
     end
@@ -116,10 +116,10 @@ describe Pattern::Deal do
         context "重複コードが登録されており、overwrites_codeが指定されているとき" do
           let(:deal_pattern) { FactoryGirl.build(:deal_pattern, :code => 'CODEX', :name => 'TEST PATTERN 2', :overwrites_code => '1') }
           it "id が変更され、編集内容が反映される" do
-            deal_pattern.prepare_overwrite.should be_truthy
-            deal_pattern.id.should == existing.id # 既存のものを更新しようとする状態になる
-            deal_pattern.debtor_entries.not_marked.size.should == 2
-            deal_pattern.creditor_entries.not_marked.size.should == 1
+            expect(deal_pattern.prepare_overwrite).to be_truthy
+            expect(deal_pattern.id).to eq existing.id # 既存のものを更新しようとする状態になる
+            expect(deal_pattern.debtor_entries.not_marked.size).to eq 2
+            expect(deal_pattern.creditor_entries.not_marked.size).to eq 1
           end
         end
       end
@@ -131,10 +131,10 @@ describe Pattern::Deal do
             deal_pattern.overwrites_code = '1'
           end
           it "id が変更され、編集内容が反映される" do
-            deal_pattern.prepare_overwrite.should be_truthy
-            deal_pattern.id.should == existing.id # 既存のものを更新しようとする状態になる
-            deal_pattern.debtor_entries.not_marked.size.should == 2
-            deal_pattern.creditor_entries.not_marked.size.should == 1
+            expect(deal_pattern.prepare_overwrite).to be_truthy
+            expect(deal_pattern.id).to eq existing.id # 既存のものを更新しようとする状態になる
+            expect(deal_pattern.debtor_entries.not_marked.size).to eq 2
+            expect(deal_pattern.creditor_entries.not_marked.size).to eq 1
           end
         end
       end
@@ -144,8 +144,8 @@ describe Pattern::Deal do
       context "２つめのNULLコードが上書き指定で登録される場合" do
         let(:deal_pattern) { FactoryGirl.build(:deal_pattern, :code => nil, :name => 'TEST PATTERN 2', :overwrites_code => '1') }
         it "上書きにならない" do
-          deal_pattern.save.should be_truthy
-          deal_pattern.id.should_not == existing.id
+          expect(deal_pattern.save).to be_truthy
+          expect(deal_pattern.id).not_to eq existing.id
         end
       end
     end
@@ -158,16 +158,16 @@ describe Pattern::Deal do
         deal_pattern.code = 'NEWCODE'
       end
       it "変更できる" do
-        deal_pattern.save.should be_truthy
+        expect(deal_pattern.save).to be_truthy
         deal_pattern.reload
-        deal_pattern.code.should == 'NEWCODE'
+        expect(deal_pattern.code).to eq 'NEWCODE'
       end
       it "コードを空文字列にするとNULLになる" do
         deal_pattern.code = ''
-        deal_pattern.save.should be_truthy
-        deal_pattern.code.should be_nil
+        expect(deal_pattern.save).to be_truthy
+        expect(deal_pattern.code).to be_nil
         deal_pattern.reload
-        deal_pattern.code.should be_nil
+        expect(deal_pattern.code).to be_nil
       end
 
     end
@@ -181,15 +181,15 @@ describe Pattern::Deal do
           deal_pattern.overwrites_code = '1'
         end
         it "saveでき、もとのオブジェクトが削除される" do
-          deal_pattern.save.should be_truthy
-          deal_pattern.id.should == existing.id
-          deal_pattern.debtor_entries.size.should == 2
-          deal_pattern.creditor_entries.size.should == 1
+          expect(deal_pattern.save).to be_truthy
+          expect(deal_pattern.id).to eq existing.id
+          expect(deal_pattern.debtor_entries.size).to eq 2
+          expect(deal_pattern.creditor_entries.size).to eq 1
           deal_pattern.reload
-          deal_pattern.id.should == existing.id
-          deal_pattern.debtor_entries.size.should == 2
-          deal_pattern.creditor_entries.size.should == 1
-          Pattern::Deal.find_by(id: old_id).should be_nil
+          expect(deal_pattern.id).to eq existing.id
+          expect(deal_pattern.debtor_entries.size).to eq 2
+          expect(deal_pattern.creditor_entries.size).to eq 1
+          expect(Pattern::Deal.find_by(id: old_id)).to be_nil
         end
       end
     end
@@ -199,7 +199,7 @@ describe Pattern::Deal do
     context "Entryがないとき" do
       let(:deal_pattern) { FactoryGirl.build(:deal_pattern, :debtor_entries_attributes => [], :creditor_entries_attributes => []) }
       it do
-        deal_pattern.should_not be_valid
+        expect(deal_pattern).not_to be_valid
       end
     end
     describe "コードの重複" do
@@ -207,19 +207,19 @@ describe Pattern::Deal do
       context "重複したコードが登録されており、overwrites_codeが指定されていないとき" do
         let(:deal_pattern) { FactoryGirl.build(:deal_pattern, :code => 'CODEX', :name => 'TEST PATTERN 2') }
         it do
-          deal_pattern.should_not be_valid
+          expect(deal_pattern).not_to be_valid
         end
       end
       context "大文字小文字の異なる重複コードが登録されており、overwrites_codeが指定されていないとき" do
         let(:deal_pattern) { FactoryGirl.build(:deal_pattern, :code => 'codeX', :name => 'TEST PATTERN 2') }
         it do
-          deal_pattern.should be_valid
+          expect(deal_pattern).to be_valid
         end
       end
       context "重複したコードが登録されており、overwrites_codeが指定されているとき" do
         let(:deal_pattern) { FactoryGirl.build(:deal_pattern, :code => 'CODEX', :name => 'TEST PATTERN 2', :overwrites_code => '1') }
         it do
-          deal_pattern.should be_valid
+          expect(deal_pattern).to be_valid
         end
       end
     end
@@ -228,7 +228,7 @@ describe Pattern::Deal do
       let(:deal_pattern) { FactoryGirl.build(:deal_pattern, :code => nil, :name => 'TEST PATTERN 2') }
 
       it "２つめのNULLコードが検証エラーとならない" do
-        deal_pattern.should be_valid
+        expect(deal_pattern).to be_valid
       end
     end
 
