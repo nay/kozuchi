0dd611a816051184c84131a3f65f6a26de360709
nay <y.ohba@everyleaf.com>
2018-11-14 06:09:51 +0900

    Use expect instead of should in spec/support/views.rb

--- spec/support/views.rb
+++ spec/support/views.rb
@@ -4,11 +4,11 @@
 # ほかのコントローラからの遷移の確認などで複数スペックで共通利用されるもの
 
 shared_examples "users/new" do
-  it {page.should have_content("ようこそ小槌へ！")}
-  it {page.should have_css("form#signupForm")}
+  it { expect(page).to have_content("ようこそ小槌へ！") }
+  it { expect(page).to have_css("form#signupForm") }
 end
 
 shared_examples "forgot_password (メール送信あり)" do
-  it {page.should have_content("登録されたEメールアドレスを入力してください。")}
+  it { expect(page).to have_content("登録されたEメールアドレスを入力してください。") }
 end
 
