28b91e9163292d057228378a7743de021e693722
nay <y.ohba@everyleaf.com>
2019-07-12 16:36:40 +0900

    change log level

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -50,7 +50,7 @@ Kozuchi::Application.configure do
 
   # Use the lowest log level to ensure availability of diagnostic information
   # when problems arise.
-  config.log_level = :info
+  config.log_level = :debug
 
   # Prepend all log lines with the following tags.
   config.log_tags = [ :request_id ]
