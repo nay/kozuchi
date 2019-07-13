588bf2ed70efa1c23b8ce1c3fe0be13f4fe057d4
nay <y.ohba@everyleaf.com>
2019-07-13 17:58:36 +0900

    add ENV for consider_all_requests_local

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -11,7 +11,7 @@ Kozuchi::Application.configure do
   config.eager_load = true
 
   # Full error reports are disabled and caching is turned on.
-  config.consider_all_requests_local       = true
+  config.consider_all_requests_local       = (ENV['CONSIDER_ALL_REQUESTS_LOCAL'] || 'false') == 'true'
   config.action_controller.perform_caching = true
 
   # Attempt to read encrypted secrets from `config/secrets.yml.enc`.
