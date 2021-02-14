782334d9b172902f65b9b8978120cc773d718b95
nay <y.ohba@everyleaf.com>
2021-02-14 10:23:21 +0900

    Move time zone and locale config under initializer

--- config/application.rb
+++ config/application.rb
@@ -27,15 +27,6 @@ module Kozuchi
     # Activate observers that should always be running.
     config.active_record.observers = :user_observer
 
-    # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
-    # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
-    # config.time_zone = 'Central Time (US & Canada)'
-    config.time_zone = 'Tokyo'
-
-    # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
-    # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    config.i18n.default_locale = 'ja'
-
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
 
